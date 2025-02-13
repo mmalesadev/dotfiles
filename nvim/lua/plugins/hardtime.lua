1f38eac897be22ce8413a873bfa87d960dfad079
Marcin Malesa <martin.malesa@gmail.com>
2025-02-13 09:25:25 +0100

    Add Nvim, Sway, Rofi, VS Code, Waybar, Kitty, Yazi and Zsh files.

---
+++ nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,18 @@
+return {
+	"m4xshen/hardtime.nvim",
+	dependencies = { "MunifTanjim/nui.nvim" },
+	opts = {
+		restricted_keys = {
+			["<Up>"] = { "" },
+			["<Down>"] = { "" },
+			["<Left>"] = { "" },
+			["<Right>"] = { "" },
+		},
+		disabled_keys = {
+			["<Up>"] = {},
+			["<Down>"] = {},
+			["<Left>"] = {},
+			["<Right>"] = {},
+		},
+	},
+}
